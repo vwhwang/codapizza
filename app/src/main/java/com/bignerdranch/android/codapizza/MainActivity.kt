6caa6e9f08b6ca2c375be5b133f0c4ef392e592b
vwhwang <vwhwang38@gmail.com>
2024-08-15 17:27:12 -0700

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/codapizza/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.codapizza
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bignerdranch.android.codapizza.ui.theme.CodaPizzaTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
