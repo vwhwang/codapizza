34b48b8a55f09a902c955c430f6c1b02d4587289
vwhwang <vwhwang38@gmail.com>
2024-08-15 19:22:26 -0700

    ch26 Jetpack compose

---
+++ app/src/main/java/com/bignerdranch/android/codapizza/ui/theme/ToppingCell.kt
@@ -0,0 +1,50 @@
+package com.bignerdranch.android.codapizza.ui.theme
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.bignerdranch.android.codapizza.model.Topping
+import com.bignerdranch.android.codapizza.model.ToppingPlacement
+
+@Composable
+fun ToppingCell(
+    topping: Topping,
+    placement: ToppingPlacement?,
+    modifier: Modifier = Modifier,
+    onClickTopping: () -> Unit,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 16.dp)
+    ) {
+        Checkbox(
+            checked = (placement != null),
+            onCheckedChange = { onClickTopping() }
+        )
+        Column(
+            modifier = modifier
+                .weight(1f, fill = true)
+                .padding(start = 4.dp)
+        ) {
+            Text(
+                text = stringResource(topping.toppingName),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            if (placement != null) {
+                Text(
+                    text = stringResource(placement.label),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
